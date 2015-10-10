e5d29dcd0e87f73a6b02c5811e78caf3bf152f3a
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-10 22:34:39 +0200

    instanciate connection on method call

---
+++ client/subscriptions.js
@@ -0,0 +1,13 @@
+Schemas = new Mongo.Collection('aaas_schemas');
+
+Meteor.subscribe('schemas', function(){
+  var schemas = Schemas.find().fetch();
+  console.log(schemas);
+
+  Schemas.find().forEach(function (schema) {
+    // instanciate the user collections
+    eval( sanitize(schema.name) + "= new Mongo.Collection(schema.name);" );
+    // subscribe to the data
+    Meteor.subscribe(schema.name);
+  });
+});
--- server/methods.js
+++ server/methods.js
@@ -5,10 +5,31 @@ Meteor.methods({
       throw new Meteor.Error(403, "You must be logged in");
     Roles.setUserRoles(this.userId, 'admin');
     Meteor.users.update( {_id: this.userId }, { $set: { mongo_url: mongo_url } } );
-    // instanciate connection to remove DB
+
+    // instanciate connection to remote DB
+    console.log('now');
+    
+    var usersapp = new MongoInternals.RemoteCollectionDriver("mongodb://127.0.0.1:3031/meteor");
+    Schemas = new Mongo.Collection("aaas_schemas", {_driver: usersapp});
+
+    Meteor.publish('schemas', function () {
+      return Schemas.find();
+    });
+
+    Schemas.find().forEach(function (schema) {
+      // instanciate the user collections
+      eval( sanitize(schema.name) + "= new Mongo.Collection(schema.name, {_driver: usersapp});" );
+      // get a reference to the collection name
+      eval("x="+sanitize(schema.name));
+      // publish it
+      Meteor.publish(schema.name, function () {
+          return x.find();
+      });
+    });
     // get list of collections
     // create simpleSchemas
     // create AdminConfig = {} using collection name
     return;
   }
 });
+
